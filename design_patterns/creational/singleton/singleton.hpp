7811f2c787626e6b5b6e2956ee901ddf6024fe97
Vatsal Gupta <@gvatsal60>
2025-06-02 15:27:31 +0000

    Added Singleton

---
+++ design_patterns/creational/singleton/singleton.hpp
@@ -0,0 +1,148 @@
+/***************************************************************************************
+ * File: singleton.hpp
+ * Author: @gvatsal60
+ * Date: 01-Jun-2025
+ * Description: This file contains the declaration of the EagerSingleton class,
+ * LazySingleton class, and ThreadSafeSingleton class, which implement the
+ * Singleton design pattern. The EagerSingleton class uses eager initialization,
+ * the LazySingleton class uses lazy initialization, and the ThreadSafeSingleton
+ * class ensures thread safety during instance creation. The Singleton design
+ * pattern ensures that a class has only one instance and provides a global
+ * point of access to that instance.
+ **************************************************************************************/
+
+/***************************************************************************************
+ * License
+ **************************************************************************************/
+/*
+ * This file is licensed under the MIT License.
+ * License information should be updated as necessary.
+ */
+
+/***************************************************************************************
+ * Includes
+ **************************************************************************************/
+#ifndef _SINGLETON_H_
+#define _SINGLETON_H_
+
+#include <cstdio>
+#include <mutex>
+
+/***************************************************************************************
+ * Constants
+ **************************************************************************************/
+
+/***************************************************************************************
+ * Structure Declarations
+ **************************************************************************************/
+
+/***************************************************************************************
+ * Class Declarations
+ **************************************************************************************/
+
+/*
+ * EagerSingleton class
+ * This class implements the Singleton design pattern using eager
+ * initialization. It ensures that only one instance of the class is created at
+ * program startup, and provides a global point of access to that instance.
+ */
+class EagerSingleton {
+ public:
+  // Deleted copy constructor and assignment operator to prevent copying
+  EagerSingleton(const EagerSingleton &) = delete;
+  EagerSingleton &operator=(const EagerSingleton &) = delete;
+
+  // Static method to access the `singleton` instance
+  static EagerSingleton &getInstance() {
+    return instance;  // Return the singleton instance
+  }
+
+ private:
+  // Constructor
+  EagerSingleton() = default;
+
+  // Destructor
+  ~EagerSingleton() = default;
+
+  static EagerSingleton instance;  // Static instance of the singleton
+};
+
+/*
+ * LazySingleton class
+ * This class implements the Singleton design pattern using lazy initialization.
+ * It ensures that only one instance of the class is created and provides a
+ * global point of access to that instance.
+ */
+class LazySingleton {
+ public:
+  // Deleted copy constructor and assignment operator to prevent copying
+  LazySingleton(const LazySingleton &) = delete;
+  LazySingleton &operator=(const LazySingleton &) = delete;
+
+  // Static method to access the `singleton` instance
+  static LazySingleton &getInstance() {  // Lazy initialization
+    if (instance == nullptr) {
+      // Create a new instance if it doesn't exist
+      instance = new LazySingleton();
+    }
+
+    return *instance;
+  }
+
+ private:
+  // Constructor
+  LazySingleton() = default;
+
+  // Destructor
+  ~LazySingleton() {
+    if (instance != nullptr) {
+      delete instance;     // Clean up the instance if needed
+      instance = nullptr;  // Set the instance pointer to null
+    }
+  }
+
+  static LazySingleton *instance;  // Pointer to the singleton instance
+};
+
+/*
+ * ThreadSafeSingleton class
+ * This class implements the Singleton design pattern with thread safety.
+ * It ensures that only one instance of the class is created, even in a
+ * multithreaded environment.
+ */
+class ThreadSafeSingleton {
+ public:
+  // Deleted copy constructor and assignment operator to prevent copying
+  ThreadSafeSingleton(const ThreadSafeSingleton &) = delete;
+  ThreadSafeSingleton &operator=(const ThreadSafeSingleton &) = delete;
+
+  // Static method to access the `singleton` instance
+  static ThreadSafeSingleton &getInstance() {
+    if (instance == nullptr) {
+      // Lock the mutex to ensure thread safety during instance creation
+      std::lock_guard<std::mutex> lock(mutex);
+      if (instance == nullptr) {
+        instance = new ThreadSafeSingleton();
+      }
+    }
+
+    return *instance;
+  }
+
+ private:
+  // Constructor
+  ThreadSafeSingleton() = default;
+
+  // Destructor
+  ~ThreadSafeSingleton() {
+    if (instance != nullptr) {
+      delete instance;     // Clean up the instance if needed
+      instance = nullptr;  // Set the instance pointer to null
+    }
+  }
+
+  static ThreadSafeSingleton *instance;  // Pointer to the singleton instance
+  static std::mutex mutex;               // Mutex for thread safety
+};
+
+#endif
